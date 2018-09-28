d7d8f6bbed00124f33419d2c4da1548b50310799
Winter Guerra <winterg@mit.edu>
2018-09-27 21:47:08 -0400

    Torrent file creator works! However, need to register the resulting torrent file.

--- downloadUtilities/createTorrentFile.js
+++ downloadUtilities/createTorrentFile.js
@@ -4,26 +4,33 @@ var createTorrent = require('create-torrent')
 var fs = require('fs')
 
 // Load list of files to put in torrent index from disk
-var fileList = fs.readFileSync('torrentFileIndexSmall.txt').toString().split("\n");
+var fileList = fs.readFileSync('../downloadUtilities/torrentFileIndex.txt').toString().split("\n");
+
+// Create streams
+var streamList = fileList.map(fPath => {
+    var stream = fs.createReadStream(fPath)
+    stream.name = fPath
+    return stream 
+});
 
 console.log(fileList)
 
-createTorrent(fileList,
+createTorrent(streamList,
     {
         // name: "BlackbirdDatasetImageRenders",            // name of the torrent (default = basename of `path`, or 1st file's name)
         name: "data",            // name of the torrent (default = basename of `path`, or 1st file's name)
         comment: "The Blackbird Dataset: A large-scale dataset for UAV perception in aggressive flight",         // free-form textual comments of the author
         announceList: [
-            ["udp://tracker.openbittorrent.com:80"],
-            ["udp://tracker.internetwarriors.net:1337"],
-            ["udp://tracker.leechers-paradise.org:6969"],
+            ["http://academictorrents.com/announce.php"],
+
             ["udp://tracker.coppersurfer.tk:6969"],
-            ["udp://exodus.desync.com:6969"],
-            ["wss://tracker.btorrent.xyz"],
-            ["wss://tracker.openwebtorrent.com"],
-            ["wss://tracker.fastcast.nz"]
+            
+            ["udp://tracker.opentrackr.org:1337/announce"],
+            
+            ["udp://tracker.leechers-paradise.org:6969"]
         ], // custom trackers (array of arrays of strings) (see [bep12](http://www.bittorrent.org/beps/bep_0012.html))
-        urlList: ["http://blackbird-dataset.mit.edu/"]        // web seed urls (see [bep19](http://www.bittorrent.org/beps/bep_0019.html))
+        urlList: ["http://blackbird-dataset.mit.edu/"],        // web seed urls (see [bep19](http://www.bittorrent.org/beps/bep_0019.html))
+        pieceLength: 128000000 // 128MB chunks to keep torrent file under 1MB 
     }, 
     function (err, torrent) {
         if (!err) {
