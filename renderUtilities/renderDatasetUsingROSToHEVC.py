71f452f8b46bf5a976136940ae310e9dad4abe45
Winter Guerra <winterg@mit.edu>
2019-10-22 18:34:06 -0400

    Updated render script to be easier to use.

--- renderUtilities/renderDatasetUsingROSToHEVC.py
+++ renderUtilities/renderDatasetUsingROSToHEVC.py
@@ -13,17 +13,9 @@ from pathos.multiprocessing import Pool
 import importlib
 from compressLosslessVideo import *
 
-#import importlib
-#from flightgogglesUtils import ImageHandler as FlightGogglesUtils
-#from blackbirdDatasetUtils import *
+defaultTrajectoryList = [ "egg", "sphinx", "halfMoon", "oval", "ampersand", "dice", "bentDice", "thrice", "tiltedThrice", "winter", "clover", "mouse", "patrick", "picasso", "sid", "star", "cameraCalibration"]
 
-
-trajectoryFolders = [ "sphinx", "halfMoon", "oval", "ampersand", "dice", "bentDice", "thrice", "tiltedThrice", "winter", "clover", "mouse", "patrick", "picasso", "sid", "star", "cameraCalibration"]
-#fileName = 'mouse'
-
-trajectoryFolders = [ "winter", "mouse", "picasso", "sid", "star"]
-
-def runRendersOnDataset(datasetFolder, renderDir, render_prefix, fps):
+def runRendersOnDataset(datasetFolder, renderDir, render_prefix, trajectoryFolders = defaultTrajectoryList, experimentList = []):
 
     devnull = open(os.devnull, 'wb') #python >= 2.4
 
@@ -41,98 +33,87 @@ def runRendersOnDataset(datasetFolder, renderDir, render_prefix, fps):
         # iterate through trajectory environments
         for experiment in config["unitySettings"][trajectoryFolder]:
 
-            # Get universal render offset and convert to values compatible with TF
-            renderOffsetArray = -1 * np.array(experiment["offset"])
-            renderOffsetTranslation = renderOffsetArray[:3]
-            theta = renderOffsetArray[3]
-            renderOffsetRotation = np.array([0,0,np.sin(theta*np.pi/360.0),np.cos(theta*np.pi/360.0)])
+            if (len(experimentList) is 0 or experiment["name"] in experimentList):
 
-            # Find all '*.bag' files in folder
-            bagFiles = glob2.glob( os.path.join(datasetFolder, trajectoryFolder, '**/*.bag') )
+                # Get universal render offset and convert to values compatible with TF
+                renderOffsetArray = -1 * np.array(experiment["offset"])
+                renderOffsetTranslation = renderOffsetArray[:3]
+                theta = renderOffsetArray[3]
+                renderOffsetRotation = np.array([0,0,np.sin(theta*np.pi/360.0),np.cos(theta*np.pi/360.0)])
 
-            # Check that this experiment is applicable to this particular subset of logs
-            subsetConstraint = experiment.get("yawDirectionConstraint","")            
-            bagFiles = [f for f in bagFiles if subsetConstraint in f]
-            # print bagFiles
-            
-            #bagFiles = [traj for traj in bagFiles if fileName in traj]
-            # print bagFiles
+                # Find all '*.bag' files in folder
+                bagFiles = glob2.glob( os.path.join(datasetFolder, trajectoryFolder, '**/*.bag') )
 
-            # Render these trajectories
-            for bagFile in bagFiles:
-
-                print "========================================"
-                print "Starting rendering of: " + bagFile
-                 
-                bagPathStem = bagFile[:-4]
-                bagDirectory = os.path.dirname(bagFile)
-
-                # Get per-trajectory centering offset.
-                offsetPath = bagPathStem + "_trajectory_offset.csv"
-                trajectoryOffsetArray = np.loadtxt(offsetPath, delimiter=',')
-                trajectoryOffsetString = " ".join( map(str,trajectoryOffsetArray) ) + " 0 0 0 1"
-                # Clean output directory
-                outputDir = "{}_{}_{}".format(bagPathStem, experiment["name"], render_prefix)
-                try:
-                   shutil.rmtree(renderDir)
-                except:
-                    pass
-                try:
-                    shutil.rmtree(outputDir)
-                except:
-                    pass
-
-                os.mkdir(outputDir)
-                os.mkdir(renderDir)
-
- 
-                # Generate timestamp file based on timestamp files left from the ISER dataset (120hz).
-                ISERDatasetPoseList = np.genfromtxt(os.path.join(bagDirectory,"csv","tf.csv"), delimiter=',',skip_header=1,usecols=[0],dtype=np.uint64)
-
-                #print ISERDatasetPoseList
-                # print ISERDatasetPoseList
+                # Check that this experiment is applicable to this particular subset of logs
+                subsetConstraint = experiment.get("yawDirectionConstraint","")            
+                bagFiles = [f for f in bagFiles if subsetConstraint in f]
+                # print bagFiles
                 
-                lastTS = 0
-                TSList = []
-                # Prune timestamps to a fps <= 120
-                frameLengthMicroseconds = (1.0e9/fps)*0.9
-                #frameLengthMicroseconds = (10.0**6/fps) -1 #*0.9
+                #bagFiles = [traj for traj in bagFiles if fileName in traj]
+                # print bagFiles
 
-                for ts in ISERDatasetPoseList:
-                    if ( (ts-lastTS) >= frameLengthMicroseconds):
-                        lastTS = ts 
-                        TSList.append(ts)
+                # Render these trajectories
+                for bagFile in bagFiles:
 
-                # Save timestamps
-                timestampsToRender = np.array(TSList)
-                timestampFile = os.path.join(renderDir, "timestampsToRender.csv") 
-                np.savetxt(timestampFile, timestampsToRender, delimiter=",", fmt='%u')
+                    print "========================================"
+                    print "Starting rendering of: " + bagFile
+                     
+                    bagPathStem = bagFile[:-4]
+                    bagDirectory = os.path.dirname(bagFile)
 
-               # Run render command
-                #command = "roslaunch flightgoggles blackbirdDataset.launch bagfile_path:='{}' output_folder:='{}' framerate:='120' scene_filename:='{}' trajectory_offset_transform:='{}' render_offset_rotation:='{}' render_offset_translation:='{}'".format(bagFile, renderDir, experiment["environment"], trajectoryOffsetString, " ".join(map(str,renderOffsetRotation)), " ".join(map(str,renderOffsetTranslation)))
-                
-                command = "roslaunch flightgoggles blackbirdDataset.launch bagfile_path:='{}' output_folder:='{}' timestampfile_path:='{}' scene_filename:='{}' trajectory_offset_transform:='{}' render_offset_rotation:='{}' render_offset_translation:='{}'".format(bagFile, renderDir, timestampFile, experiment["environment"], trajectoryOffsetString, " ".join(map(str,renderOffsetRotation)), " ".join(map(str,renderOffsetTranslation)))
+                    # Get per-trajectory centering offset.
+                    offsetPath = bagPathStem + "_trajectory_offset.csv"
+                    trajectoryOffsetArray = np.loadtxt(offsetPath, delimiter=',')
+                    trajectoryOffsetString = " ".join( map(str,trajectoryOffsetArray) ) + " 0 0 0 1"
+                    # Clean output directory
+                    outputDir = "{}_{}_{}".format(bagPathStem, experiment["name"], render_prefix)
+                    try:
+                       shutil.rmtree(renderDir)
+                    except:
+                        pass
+                    try:
+                        shutil.rmtree(outputDir)
+                    except:
+                        pass
 
-                print command
+                    os.mkdir(outputDir)
+                    os.mkdir(renderDir)
 
-                process = subprocess.Popen(command, shell=True) #, stdout=devnull)
-                process.wait()
+     
+                    # Generate timestamp file based on timestamp files left from the ISER dataset (120hz).
+                    # NOTE: This file might not exist if the rosbag file is new (Egg trajectories).
+                    
+                    ISERDataset120HzTimestamps = np.genfromtxt(os.path.join(bagDirectory,"csv","camera_l_slash_camera_info.csv"), delimiter=',',skip_header=1,usecols=[0],dtype=np.uint64)
 
-                # Loop through output folders and compress files (in parallel).
-                def compressVideo(_folder_name):
-                    subfolder_name = _folder_name.split('/')[-2]
-                    #print subfolder_name
+                    timestampFile = os.path.join(renderDir, "timestampsToRender.csv") 
+                    np.savetxt(timestampFile, ISERDataset120HzTimestamps, delimiter=",", fmt='%u')
 
-                    # Compress files and move to final destination
-                    print "Compressing images to lossless HEVC"
-                    compressLosslessVideo(_folder_name, ".ppm", output_folder=os.path.join(outputDir, subfolder_name))           
+                   # Run render command
+                    #command = "roslaunch flightgoggles blackbirdDataset.launch bagfile_path:='{}' output_folder:='{}' framerate:='120' scene_filename:='{}' trajectory_offset_transform:='{}' render_offset_rotation:='{}' render_offset_translation:='{}'".format(bagFile, renderDir, experiment["environment"], trajectoryOffsetString, " ".join(map(str,renderOffsetRotation)), " ".join(map(str,renderOffsetTranslation)))
+                    
+                    command = "roslaunch flightgoggles blackbirdDataset.launch bagfile_path:='{}' output_folder:='{}' timestampfile_path:='{}' scene_filename:='{}' trajectory_offset_transform:='{}' render_offset_rotation:='{}' render_offset_translation:='{}'".format(bagFile, renderDir, timestampFile, experiment["environment"], trajectoryOffsetString, " ".join(map(str,renderOffsetRotation)), " ".join(map(str,renderOffsetTranslation)))
 
-                #p = Pool()
-                #map(compressVideo, glob.glob(os.path.join(renderDir, "*/")))
+                    print command
 
-                for d in glob.glob(os.path.join(renderDir, "*/")):
-                    compressVideo(d)
-		time.sleep(2)
+                    process = subprocess.Popen(command, shell=True) #, stdout=devnull)
+                    process.wait()
+
+                    # Loop through output folders and compress files (in parallel).
+                    def compressVideo(_folder_name):
+                        subfolder_name = _folder_name.split('/')[-2]
+                        #print subfolder_name
+
+                        # Compress files and move to final destination
+                        print "Compressing images to lossless HEVC"
+                        compressLosslessVideo(_folder_name, ".ppm", output_folder=os.path.join(outputDir, subfolder_name))           
+
+                    #p = Pool()
+                    #map(compressVideo, glob.glob(os.path.join(renderDir, "*/")))
+
+                    for d in glob.glob(os.path.join(renderDir, "*/")):
+                        compressVideo(d)
+                    
+                    time.sleep(2)
 
 
 if __name__ == '__main__':
--- renderUtilities/runRenders.sh
+++ renderUtilities/runRenders.sh
@@ -1,4 +1,4 @@
 # Render datasets at 60hz
-./renderDatasetUsingROSToHEVC.py /media/medusa/NAS/Datasets/IJRRBlackbirdDataset/v3/BlackbirdDatasetData/ /home/medusa/render_temp/ "ijrr_sync_rt_linux_1" 120
+./renderDatasetUsingROSToHEVC.py --datasetFolder /media/medusa/NAS/Datasets/IJRRBlackbirdDataset/v3/BlackbirdDatasetData/ --renderDir /home/medusa/render_temp/ --render-prefix "ijrr_sync_rt_linux_1" --experimentList ["NYC_Subway_Station_Moving_Train"]  
 
 #./renderDatasetUsingROSToHEVC.py /media/medusa/RAID/IJRRBlackbirdDataset/v2/BlackbirdDatasetData/ /home/medusa/render_temp/ "_120hz_try_4" 121
