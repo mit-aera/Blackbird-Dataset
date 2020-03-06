68485ab2977d3eb55a8f2c7fe3f09d4d239a01d7
Winter Guerra <winterg@mit.edu>
2020-03-06 03:16:15 -0500

    Added some data helpers

--- fileTreeUtilities/checkRenders.sh
+++ fileTreeUtilities/checkRenders.sh
@@ -2,82 +2,118 @@
 
 # Winter Guerra <winterg@mit.edu>
 # Requires one folder argument.
-# Checks that 60Hz 
+# Finds all subfolders with renders and checks that each render has all frames for all RGBD feeds.
 
 
-
-# Get segmentation folders
-SEGMENTATION_FOLDERS=`find -L $1 -iname "*_Segmented"`
-
-# rosbag_Ancient_Asia_Museum_Room_ijrr_new_raycast_depth_5
+# Get folders of reference Grayscale renders
+GRAY_FOLDERS=`find -L $1 -iname "*Left_Gray"`
 
 # Prepare output file with list of good depth renders
-echo "" > badSegRenders.txt
-echo "" > badRGBRenders.txt
-echo "" > goodDepthRenders.txt
-echo "" > badDepthRenders.txt
-
+echo "" > badGrayRenders.txt
+echo "" > goodNewRenders.txt
+echo "" > badNewRenders.txt
 
 # For each folder:
-for FOLDER in $SEGMENTATION_FOLDERS; do
+for GRAY_FOLDER in $GRAY_FOLDERS; do
 
-# Check that the depth folder exists
-EXPERIMENT=`echo $FOLDER | awk -F '/' '{print $(NF-1)}'`
-CAMERA=`echo $FOLDER | awk -F '/' '{print $NF}' | sed s%_Segmented%%`
-DEPTH_FOLDER=${FOLDER}/../../rosbag_${EXPERIMENT}_ijrr_new_raycast_depth_5/${CAMERA}_Depth
-FINAL_FOLDER_DEST=${FOLDER}/../
+  EXPERIMENT=`echo $GRAY_FOLDER | awk -F '/' '{print $(NF-1)}'`
+  CAMERA=`echo $GRAY_FOLDER | awk -F '/' '{print $NF}' | sed s%_Gray%%`
+  NEW_FOLDER=${GRAY_FOLDER}/../../rosbag_${EXPERIMENT}_ijrr_new_full_suite_1/${CAMERA}_RGB
+  FINAL_FOLDER_DEST=`realpath ${GRAY_FOLDER}/../`
+  
+  # Get number of Grayscale frames
+  GT_NUM_FRAMES=`wc -l < ${FINAL_FOLDER_DEST}/120HzTimestamps.csv`
+  EXPECTED_120_FRAMES=$(( GT_NUM_FRAMES - 20 )) # Add a buffer
+  EXPECTED_60_FRAMES=$(( GT_NUM_FRAMES / 2 ))
+  EXPECTED_60_FRAMES=$(( EXPECTED_60_FRAMES - 20 )) # Add a buffer
+  
+  # Check that Current RGB & Depth renders have the correct number of frames
+  GRAY_NUM_IMAGES=`wc -l < ${FINAL_FOLDER_DEST}/${CAMERA}_Gray/video_frame_n_sec_timestamps.txt`
+  
 
-NUM_GRAY_FRAMES=`cat ${FINAL_FOLDER_DEST}/120HzTimestamps.csv | wc -l`
-EXPECTED_FRAMES=$(( NUM_GRAY_FRAMES / 2 ))
-EXPECTED_FRAMES=$(( EXPECTED_FRAMES - 20 )) # Add a buffer
+  # Check that the new renders have the correct number of frames (if folder exists)
+  NEW_RGB_NUM_IMAGES=0
+  if [ -d $NEW_FOLDER ]; then
+    # Check number of new images
+    NEW_RGB_NUM_IMAGES=`wc -l < ${NEW_FOLDER}/video_frame_n_sec_timestamps.txt`
+  fi
+    
+  # Check that the new renders have the correct number of frames (if folder exists)
+  RGB_NUM_IMAGES=0
+  if [ -d ${FINAL_FOLDER_DEST}/${CAMERA}_RGB ]; then
+    # Check number of new images
+    RGB_NUM_IMAGES=`wc -l < ${FINAL_FOLDER_DEST}/${CAMERA}_RGB/video_frame_n_sec_timestamps.txt`
+  fi
 
-# Check that RGB, Semantic, & Depth renders have the correct number of frames
-RGB_NUM_IMAGES=`cat ${FINAL_FOLDER_DEST}/${CAMERA}_RGB/video_frame_n_sec_timestamps.txt | wc -l`
-SEG_NUM_IMAGES=`cat ${FOLDER}/video_frame_n_sec_timestamps.txt | wc -l`
-
-if [ ! -d $DEPTH_FOLDER ]; then
-  #echo "WARNING! $DEPTH_FOLDER DOES NOT exist." # Silent error for now
-  #echo "$FINAL_FOLDER_DEST" >> badDepthRenders.txt
-  #continue
+  # Check that the new renders have the correct number of frames (if folder exists)
   DEPTH_NUM_IMAGES=0
-else
-  # Check number of depth images
-  DEPTH_NUM_IMAGES=`cat $DEPTH_FOLDER/video_frame_n_sec_timestamps.txt | wc -l`
-fi
-  
-echo "RGB: $RGB_NUM_IMAGES, Seg: $SEG_NUM_IMAGES, Depth: $DEPTH_NUM_IMAGES, GT with buffer: $EXPECTED_FRAMES"
+  if [ -d ${FINAL_FOLDER_DEST}/${CAMERA}_Depth ]; then
+    # Check number of new images
+    DEPTH_NUM_IMAGES=`wc -l < ${FINAL_FOLDER_DEST}/${CAMERA}_Depth/video_frame_n_sec_timestamps.txt`
+  fi
 
-
-# Check if Depth render maps correctly to ground truth
-if (( "$DEPTH_NUM_IMAGES" != "$RGB_NUM_IMAGES" )); then
-  echo "WARNING! RGB: $RGB_NUM_IMAGES, Seg: $SEG_NUM_IMAGES, Depth: $DEPTH_NUM_IMAGES, GT with buffer: $EXPECTED_FRAMES"
+  echo "Expected Frames from GT: $EXPECTED_60_FRAMES, Orig Gray: $GRAY_NUM_IMAGES, Orig RGB: $RGB_NUM_IMAGES, Orig Depth: $DEPTH_NUM_IMAGES, New: $NEW_RGB_NUM_IMAGES"
   
-  # Check that depth image number >= ground truth expected
-  if (( "$DEPTH_NUM_IMAGES" < "$EXPECTED_FRAMES" )); then
-    echo "Depth needs to be rerendered."
-    echo "$FINAL_FOLDER_DEST" >> badDepthRenders.txt
-  else
-    # Depth is actually good!
-    echo "$DEPTH_FOLDER $FINAL_FOLDER_DEST" >> goodDepthRenders.txt
-  fi 
-  
-  # Check that segmented image number >= ground truth expected
-  if (( "$SEG_NUM_IMAGES" < "$EXPECTED_FRAMES" )); then
-    echo "Segmented needs to be rerendered."
-    echo "$FOLDER" >> badSegRenders.txt
-  fi 
- 
-  # Check that rgb image number >= ground truth expected
-  if (( "$RGB_NUM_IMAGES" < "$EXPECTED_FRAMES" )); then
-    echo "RGB needs to be rerendered."
-    echo "${FINAL_FOLDER_DEST}/${CAMERA}_RGB" >> badRGBRenders.txt
-  fi 
-   
-else
-  # Output file is of form "render_loc final_parent_dir"
-  echo "$DEPTH_FOLDER $FINAL_FOLDER_DEST" >> goodDepthRenders.txt
-fi
+  # Cases to check:
+  #########
+  # Originals are good: (Gray >= 120Hz minimum) && (Orig RGB >=  60Hz minimum ) && (Orig Depth == Orig RGB )
+  # New renders are good: (Gray >= 120Hz minimum) && (New RGB >=  60Hz minimum ) 
+  ### if something is wrong: ###
+  # Orig Gray bad: (Gray < 120Hz Minimum)
+  # New renders are bad: (New RGB <  60Hz minimum )
+  ####
 
-echo "Processed $FOLDER"
+  # Check that 
+  if (( "$GRAY_NUM_IMAGES" >= "$EXPECTED_120_FRAMES" )) && (( "$RGB_NUM_IMAGES" >= "$EXPECTED_60_FRAMES" )) && (( "$DEPTH_NUM_IMAGES" == "$RGB_NUM_IMAGES" )); then
+    # THE ORIGINALS ARE GOOD!
+    echo Good!
+  elif (( "$GRAY_NUM_IMAGES" >= "$EXPECTED_120_FRAMES" )) && (( "$NEW_RGB_NUM_IMAGES" >= "$EXPECTED_60_FRAMES" )); then
+    # The new renders are good!
+    echo Good!
+    echo "`realpath $NEW_FOLDER` $FINAL_FOLDER_DEST" >> goodNewRenders.txt
+
+  else 
+	  
+    if (( "$GRAY_NUM_IMAGES" < "$EXPECTED_120_FRAMES" )); then
+      # The original gray images are bad
+      echo Bad original grayscale images!
+      echo "$FOLDER" >> badGrayOriginals.txt
+
+    fi 
+    if (( "$NEW_RGB_NUM_IMAGES" < "$EXPECTED_60_FRAMES" )); then
+      # The new renders are bad
+      echo Bad new renders!
+      echo "$FINAL_FOLDER_DEST" >> badNewRenders.txt
+    fi
+  
+  fi
+
+   # # Check that depth image number >= ground truth expected
+   # if (( "$DEPTH_NUM_IMAGES" < "$EXPECTED_60_FRAMES" )); then
+   #   echo "Depth needs to be rerendered."
+   #   echo "$FINAL_FOLDER_DEST" >> badDepthRenders.txt
+   # else
+   #   # Depth is actually good!
+   #   echo "$NEW_FOLDER $FINAL_FOLDER_DEST" >> goodDepthRenders.txt
+   # fi 
+   # 
+   # # Check that segmented image number >= ground truth expected
+   # if (( "$SEG_NUM_IMAGES" < "$EXPECTED_60_FRAMES" )); then
+   #   echo "Segmented needs to be rerendered."
+   #   echo "$FOLDER" >> badSegRenders.txt
+   # fi 
+   #
+   # # Check that rgb image number >= ground truth expected
+   # if (( "$RGB_NUM_IMAGES" < "$EXPECTED_60_FRAMES" )); then
+   #   echo "RGB needs to be rerendered."
+   #   echo "${FINAL_FOLDER_DEST}/${CAMERA}_RGB" >> badRGBRenders.txt
+   # fi 
+     
+  #else
+  #  # Output file is of form "render_loc final_parent_dir"
+  #  echo "$NEW_FOLDER $FINAL_FOLDER_DEST" >> goodDepthRenders.txt
+  #fi
+  
+  echo "Processed $FOLDER"
 
 done
